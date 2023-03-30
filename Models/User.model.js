ee86652f0fbaddc08e042d11ae00d00a588e8161
Akash Das <akashdas0599@gmail.com>
2023-03-30 19:22:33 +0530

    feat: JWT access token

--- Models/User.model.js
+++ Models/User.model.js
@@ -27,6 +27,14 @@ UserSchema.pre("save", async function (next) {
   }
 });
 
+UserSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    next(error);
+  }
+};
+
 const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
--- Routes/Auth.js
+++ Routes/Auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const createError = require("http-errors");
 const User = require("../Models/User.model");
 const { authSchema } = require("../helpers/validation_schema");
+const { signAccessToken } = require("../helpers/jwt_helper");
 
 router.post("/register", async (req, res, next) => {
   try {
@@ -13,14 +14,31 @@ router.post("/register", async (req, res, next) => {
       throw createError.Conflict(`${result.email} already registered`);
 
     const user = await User.create(result);
-    res.send(user);
+
+    const accessToken = await signAccessToken(user.id);
+    res.send({ accessToken });
   } catch (error) {
     if (error.isJoi === true) error.status = 422;
     next(error);
   }
 });
 router.post("/login", async (req, res, next) => {
-  res.send("Login route");
+  try {
+    const result = await authSchema.validateAsync(req.body);
+
+    const user = await User.findOne({ email: result.email });
+
+    if (!user) throw createError.NotFound("User not Registered");
+    const isMatch = await user.isValidPassword(result.password);
+
+    if (!isMatch) throw createError.Unauthorized("username/password not valid");
+    const accessToken = await signAccessToken(user.id);
+    res.send({ accessToken });
+  } catch (error) {
+    if (error.isJoi === true)
+      return next(createError.BadRequest("Invalid Username/Password"));
+    next(error);
+  }
 });
 router.post("/refresh-token", async (req, res, next) => {
   res.send("Refresh route");
