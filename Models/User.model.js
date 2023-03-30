d778a10de57f095b537008cedf82001aa947dcda
Akash Das <akashdas0599@gmail.com>
2023-03-30 16:22:32 +0530

    feat: Hashed password using bcrypt

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,4 +1,7 @@
+const e = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -12,6 +15,16 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model("user", UserSchema);
